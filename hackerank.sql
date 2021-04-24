94215ffa5723c62529e5e21b99ff050512ca46ca
Emin Mammadov <emammadov@acerta.ca>
2021-04-24 16:46:07 +0400

    sql

--- hackerank.sql
+++ hackerank.sql
@@ -75,3 +75,108 @@ select name
 from students
 where marks>75
 order by right(name, 3), ID ASC
+
+/*
+Write a query identifying the type of each record in the TRIANGLES table using its three side lengths. 
+utput one of the following statements for each record in the table:
+Equilateral: It's a triangle with  sides of equal length.
+Isosceles: It's a triangle with  sides of equal length.
+Scalene: It's a triangle with  sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+
+SELECT CASE WHEN A + B <= C OR B + C <= A OR A + C <= B then "Not A Triangle"
+            WHEN A=B and B=C and A=C then "Equilateral"
+            WHEN A=B or B=C or A=C then "Isosceles"
+            ELSE "Scalene"
+        end
+from TRIANGLES;
+
+/*
+Generate the following two result sets:
+
+1. Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter 
+of each profession as a parenthetical (i.e.: enclosed in parentheses). 
+For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+
+
+2. Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format:
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+*/
+
+select CONCAT(name, '(', left(occupation, 1), ')') as task_1
+from occupations
+order by name ASC;
+
+select concat('There are a total of ', count(occupation), ' ', lower(occupation), 's.')
+from occupations
+group by occupation
+order by count(occupation);
+
+/*
+Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and 
+displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+
+Note: Print NULL when there are no more names corresponding to an occupation.
+
+Input Format
+
+The OCCUPATIONS table is described as follows:
+*/
+
+/*Review Again*/
+
+/*https://stackoverflow.com/questions/38759662/not-able-to-understand-this-sql-query*/
+SET @d=0, @a=0, @p=0, @s=0;
+SELECT MIN(Doctor),MIN(Professor),MIN(SINGER),MIN(Actor)
+FROM
+(SELECT IF(OCCUPATION='Actor',NAME,NULL) AS Actor, 
+        IF(OCCUPATION='Doctor',NAME,NULL) AS Doctor, 
+        IF(OCCUPATION='Professor',NAME,NULL) AS Professor, 
+        IF(OCCUPATION='Singer',NAME,NULL) AS SINGER,
+    case OCCUPATION when 'ACTOR' THEN @a:=@a+1 
+                    when 'Doctor' THEN @d:=@d+1 
+                    when 'Professor' THEN @p:=@p+1 
+                    when 'Singer' THEN @s:=@s+1 
+    end 
+    as idn FROM OCCUPATIONS ORDER BY NAME ) AS TMP GROUP BY TMP.idn ;
+
+        
+/*
+Given the table schemas below, 
+write a query to print the company_code, founder name, total number of lead managers, total number of senior managers, total number of managers, and total number of employees. Order your output by ascending company_code.
+*/
+
+/*REDO WITH JOINs*/
+
+select C.Company_Code, C.founder, count(distinct(LM.lead_manager_code)), count(distinct(SM.senior_manager_code)), count(distinct(M.manager_code)), count(distinct(E.employee_code))
+from company as C, Lead_Manager as LM, Senior_Manager as SM, Manager as M, Employee as E
+where c.company_code = lm.company_code 
+      and lm.lead_manager_code=sm.lead_manager_code
+      and sm.senior_manager_code=m.senior_manager_code 
+      and m.manager_code=e.manager_code
+group by C.company_code, C.founder
+order by C.company_code;
+
+/*
+Samantha was tasked with calculating the average monthly salaries for all employees in the EMPLOYEES table, but did
+not realize her keyboard's  key was broken until after completing the calculation. She wants your help finding the
+difference between her miscalculation (using salaries with any zeros removed), and the actual average salary.
+
+Write a query calculating the amount of error (i.e.:  average monthly salaries), and round it up to the next integer.
+*/
+
+SELECT ceiling(avg(Salary)-avg(REPLACE(Salary,0,'')))
+from employees;
+
+
+/*
+Write a query to find the maximum total earnings for all employees as well as the total number of employees who have maximum total earnings. 
+*/
+
+select max(salary*months), count(salary*months) as salary_per_month 
+from Employee
+group by salary*months
+order by salary_per_month desc
+limit 1;
